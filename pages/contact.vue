f1be8c9dd8a3183bfd73d93e31fe33cce0646bb1
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-19 09:25:35 +0100

    replaces nuxt-link to a tag for external link

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="card">
-    <p class="card-body">  You can find me here <nuxt-link to="https://twitter.com/nezlicodes"><font-awesome-icon :icon="['fab', 'twitter']"/> </nuxt-link> </p>
+    
+    <p class="card-body">  You can find me here <a href="https://twitter.com/nezlicodes"><font-awesome-icon :icon="['fab', 'twitter']"/> </a> </p>
     </div>
 </template>
