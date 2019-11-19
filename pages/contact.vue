b9dd6f1597bf3bc06db9ae9b0ae037c5cb184f9c
nezlicodes <benmeradi.lilyanezli@gmail.com>
2019-11-19 09:38:48 +0100

    adds head to conact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,30 @@
 <template>
     <div class="card">
     
-    <p class="card-body">  You can find me here <a href="https://twitter.com/nezlicodes"><font-awesome-icon :icon="['fab', 'twitter']"/> </a> </p>
+    <p class="card-body">  You can find me here <a href="https://twitter.com/nezlicodes">
+    <font-awesome-icon :icon="['fab', 'twitter']" size = "lg"/> </a> 
+    <a class="github" href="https://github.com/nezlicodes"><font-awesome-icon :icon="['fab', 'github']" size = "lg"/> </a>
+ </p>
+    
     </div>
 </template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Contact',
+            meta: [
+                {name: 'description', content: 'links to contact me'},
+                {name: 'keywords', content:'twitter, github, nezlicodes'}
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="css">
+    .github{
+        color: black
+    }
+</style>
